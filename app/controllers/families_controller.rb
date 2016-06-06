eb59f672296892bb29dadd38d362a5bc7edb33ef
joshuajhun <joshuajhun@gmail.com>
2016-06-06 11:32:50 -0600

    loved your application The app looked great! I loved the idea and the structure behind it. Great use of the twitter gem and breaking twitter off into its own service object. I liked the schema structure and your desire to keep pushing logic down the stack. The only thing I would have you consider is maybe looking into presenter classes to pull some of that logic out of the view. Great work!

--- app/controllers/families_controller.rb
+++ app/controllers/families_controller.rb
@@ -1,5 +1,17 @@
 class FamiliesController < ApplicationController
+# you can look into scoping this down the current user by creating a before action for
+# the current user.
+#so instead of having the if/else statement in the families to take care of the current_user
+# you can deal with it as the user visits your page.
+# --------------------------
 
+  #before_action :require_authenticated_user
+
+  #def require_authenticated_user
+    #redirect_to login_path flash[:login] ="Must Login" unless current_user
+  #end
+
+# --------------------------
   def index
     @families = Family.to_arrive
   end
